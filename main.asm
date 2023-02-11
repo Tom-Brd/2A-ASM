7a048b71227f7bc1d552cc936dfafe782bbeb135
Tom-Brd <bourlard.tom.94@gmail.com>
2023-02-11 22:28:39 +0100

    move rand.asm

--- main.asm
+++ main.asm
@@ -6,17 +6,6 @@ extern scanf
 %define WORD	2
 %define BYTE	1
 
-global rand
-
-; la fonction permet de diviser le nombre aléatoire
-rand:
-    rdrand eax ;on stock le nombre aléatoire sur 32 bits dans eax
-    mov ecx, 0
-    mov ecx, edi ;On copie la valeur de l'argument dans rax
-    xor edx, edx ;on met 0 dans le registre edx
-    div ecx ;le reste de la division est contenu dans edx --> valeur modulo
-    ret
-
 global main
 
 section .bss
