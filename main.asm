f4207251b81a36800521c637715ba455fdd1d3d3
Tom-Brd <bourlard.tom.94@gmail.com>
2023-02-11 19:01:01 +0100

    amend

--- main.asm
+++ main.asm
@@ -1,60 +1,41 @@
-extern printf
-extern scanf
-
-%define QWORD	8
-%define DWORD	4
-%define WORD	2
-%define BYTE	1
-
-global main
-
-section .bss
-coordonnees:    resd    6
-
-
 section .data
-affichage:  db  "Valeur générée : %d", 10, 0
+    max_value dd 0        ; Valeur maximale pour les nombres aléatoires
+    random_numbers times 6 dd 0 ; Tableau pour stocker les nombres aléatoires
 
+section .text
+    global _start
 
-section .txt
+; Génération de nombres aléatoires en utilisant RDRAND
+; Entrée: EAX - Valeur maximale pour les nombres aléatoires
+; Sortie: EAX - Nombre aléatoire généré
 
-global rand
+random_number:
+    push ebp        ; Sauvegarder le sommet de pile
+    mov ebp, esp    ; Initialiser le sommet de pile
 
-; la fonction permet de diviser le nombre aléatoire
-rand:
-    rdrand eax ;on stock le nombre aléatoire sur 32 bits dans eax
-    mov ecx, 0
-    mov ecx, edi ;On copie la valeur de l'argument dans rax
-    xor edx, edx ;on met 0 dans le registre edx
-    div ecx ;le reste de la division est contenu dans edx --> valeur modulo
-ret
+    ; Tenter de générer un nombre aléatoire avec RDRAND
+    rdrand eax      ; Générer un nombre aléatoire avec RDRAND
+    xor edx, edx    ; Initialiser EDX pour la division
+    div dword [ebp + 8] ; Diviser le nombre aléatoire par max_value pour limiter sa valeur entre 0 et max_value
 
-main:
+    pop ebp        ; Restaurer le sommet de pile
+    ret            ; Retourner le nombre aléatoire généré
 
-    mov r8d, 400 ;largeur de la fenêtre
-    mov edx, 0
-    mov esi, 0 ;compteur
+; Programme principal
+_start:
+    mov eax, [max_value] ; Récupérer la valeur maximale pour les nombres aléatoires
 
-    boucle:
+    ; Générer 6 nombres aléatoires et les stocker dans le tableau random_numbers
+    mov ecx, 6      ; Initialiser le compteur pour la boucle
+    mov ebx, 0      ; Initialiser l'index du tableau
+generate_random_numbers:
+    push eax        ; Pousser la valeur maximale pour les nombres aléatoires sur la pile
+    call random_number ; Appeler la fonction random_number pour générer un nombre aléatoire
+    mov [random_numbers + ebx], eax ; Stocker le nombre aléatoire dans le tableau
+    add ebx, 4      ; Incrémenter l'index du tableau de 4 octets
+    loop generate_random_numbers ; Répéter la boucle jusqu'à ce que le compteur soit épuisé
 
-        mov edi, r8d
-        mov rax, 0
-        call rand
-
-        mov dword[coordonnees + esi*DWORD], edx
-
-        mov rdi,affichage ;
-        movzx rsi,dword[coordonnees + esi*DWORD]
-        mov rax,0
-        call printf
-
-        inc esi
-        cmp esi, 6
-        jne boucle
-
-
-    ;fin du programme
-    mov rax, 60
-    mov rdi, 0
-    syscall
-    ret
+    ; Terminer le programme
+    mov eax, 1      ; Code de sortie pour le système d'exploitation
+    xor ebx, ebx    ; Initialiser EBX pour la sortie
+    int 0x80        ; Appel système pour terminer le programme
